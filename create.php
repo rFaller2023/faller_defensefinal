82a222952d36374d71679a9afaa59762734cf2a7
rosemarie <rfaller989@gmail.com>
2023-06-03 11:46:01 +0800

    add

--- create.php
+++ create.php
@@ -1,18 +1,27 @@
 <?php
-include 'database.php';
-class Employee extends Database 
+include 'index.php';
+class Anime extends Database 
 {
-    public $tblName="employee";
+    public $tblName="anime";
 
-    public function __construct()
+    public function table()
     {
     
         $create="CREATE TABLE IF NOT EXISTS $this->tblName (
             id int primary key auto_increment,
-            first_name varchar(200),
-            last_name varchar(200)
+            anime_name varchar(200),
+            manga_author varchar(200)
             )";
-
+    $this->conn->query($create);
            
 }
+public function insert()
+{
+    $in="INSERT INTO $this->tblName
+    VALUES (NULL,'One Piece','Eiichiro Oda')";
+    $this->conn->query($in);
+    var_dump($this->conn->error);
 }
+}
+$r= new Anime();
+$r->table();
